da5bd31e9fb619088bec3c738b9b774d26909c34
loghmanb <loghmanb@gmail.com>
2023-07-08 06:21:33 +0100

    Add necessare files for frontend app

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    path("", include("frontend.urls")),
     path("admin/", admin.site.urls),
 ]
