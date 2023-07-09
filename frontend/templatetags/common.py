d1282dfddbaad35e30e1e89dd6fdd7521d73c1fe
loghmanb <loghmanb@gmail.com>
2023-07-09 12:16:58 +0100

    refine templates and fix linting errors

--- frontend/templatetags/common.py
+++ frontend/templatetags/common.py
@@ -1,21 +1,25 @@
+"""Module for common tags and filters."""
 from django import template
 
 
 register = template.Library()
 
 
-@register.inclusion_tag('frontend/components/hint.html')
+@register.inclusion_tag('frontend/components/base_component/hint.html')
 def hint(string: str):
+    """Hint tag."""
     return {"hint": string, }
 
 
-@register.inclusion_tag("frontend/components/field_error.html")
+@register.inclusion_tag("frontend/components/base_component/field_error.html")
 def field_error(string: str):
+    """Field error tag."""
     return {"error": string, }
 
 
 @register.filter(is_safe=True)
 def item(options: dict | list, idx: int | str) -> any:
+    """Item filter."""
     if options:
         if isinstance(options, dict):
             return options.get(idx)
