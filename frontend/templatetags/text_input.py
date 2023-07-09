e1b6724b174c27301101f8b04de3a7dc846aa9ee
loghmanb <loghmanb@gmail.com>
2023-07-09 12:20:11 +0100

    fix linting issues for input text component

--- frontend/templatetags/text_input.py
+++ frontend/templatetags/text_input.py
@@ -1,3 +1,4 @@
+"""Module for InputText component."""
 from django import template
 
 from ..common import Field
@@ -5,7 +6,10 @@ from ..common import Field
 register = template.Library()
 
 
-@register.inclusion_tag('frontend/components/text_input.html', takes_context=True)
+@register.inclusion_tag(
+        'frontend/components/text_input.html',
+        takes_context=True)
 def text_input(context: dict, field: Field) -> dict[str, Field]:
+    """Input text tag."""
     context['field'] = field
     return context
