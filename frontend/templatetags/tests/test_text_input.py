1fe5c0e9581b0fbb9e3cf76498b836872086a027
Loghman Barari <loghmanb@gmail.com>
2023-07-14 07:15:34 +0000

    add test_input_text_if_one_question

--- frontend/templatetags/tests/test_text_input.py
+++ frontend/templatetags/tests/test_text_input.py
@@ -2,14 +2,16 @@
 from project.utils import BaseTestCase
 from ...common import Field, FieldType
 
+
 class InputTextTest(BaseTestCase):
     """Text Input test case."""
+
     def setUp(self) -> None:
         """Setup."""
         self.sample_template = (
             '{% load text_input %}'
             '{% text_input field %}'
-            )
+        )
 
     def test_input_text_simple(self):
         """Test if version env variable is not set."""
@@ -20,4 +22,18 @@ class InputTextTest(BaseTestCase):
             question="What is your home address?",
         )
         rendered = self.render_template(self.sample_template, {'field': field})
-        self.assertEqual(rendered, '<link href="foo/bar.css?"__v__=1" rel="stylesheet">')
+        self.assertEqual(rendered, '<div class="govuk-form-group ">\n    \n\n<label class="govuk-label" for="address-line-id">\n  \n</label>\n\n\n\n\n\n<div class="govuk-input__wrapper">\n  \n  <input type="text" \n    class="govuk-input  govuk-input--width-None " \n    id="address-line-id" name="address-line-name"  autocomplete="None"\n    maxlength=""\n    spellcheck="">\n  \n</div>\n\n</div>\n')
+
+    def test_input_text_if_one_question(self):
+        """Test if version env variable is not set."""
+        field = Field(
+            "address-line-id",
+            "address-line-name",
+            FieldType.INPUT_TEXT,
+            question="What is your home address?",
+        )
+        rendered = self.render_template(
+            self.sample_template,
+            {'field': field, 'one_question': True},
+        )
+        self.assertEqual(rendered, '<div class="govuk-form-group ">\n    \n\n<h1 class="govuk-label-wrapper">\n  <label class="govuk-label govuk-label--l" for="address-line-id">\n    What is your home address?\n  </label>\n</h1>\n\n\n\n\n\n<div class="govuk-input__wrapper">\n  \n  <input type="text" \n    class="govuk-input  govuk-input--width-None " \n    id="address-line-id" name="address-line-name"  autocomplete="None"\n    maxlength=""\n    spellcheck="">\n  \n</div>\n\n</div>\n')
