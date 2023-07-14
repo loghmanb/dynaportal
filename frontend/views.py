f8fa56479cdded0cc0b2b9d2f2401c065ef27e14
Loghman Barari <loghmanb@gmail.com>
2023-07-14 18:21:00 +0000

    add sample data for checkboxes

--- frontend/views.py
+++ frontend/views.py
@@ -9,11 +9,21 @@ def home(request):
             "address-line-123",
             "address-line-1",
             FieldType.INPUT_TEXT,
+            "Home Address",
             question="What is your home address?",
-            size=5,
             hint="The name you’ll use on promotional material 123",
             error="Enter an event name 123",
-            options={"input_prefix": "£", "input_suffix": "per item 123", "is_code": True, "spellchecker": False}),
+            options={"size":5,"input_prefix": "£", "input_suffix": "per item 123", "is_code": True, "spellchecker": False}),
+        Field(
+            "countries-id",
+            "countries",
+            FieldType.CHECKBOXES,
+            "What is your nationality?",
+            hint="If you have dual nationality, select all options that are relevant to you.",
+            options={
+                "items": ["France", "Portugal","Spain", ]
+            }
+        ),
     ]
 
     return render(request, 'frontend/home.html', context={"fields": fields, 'one_question': len(fields)==1})
