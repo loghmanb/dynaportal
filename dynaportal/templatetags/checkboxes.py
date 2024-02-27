f35318f7743934d07259cb47c9bc79bc7387fa8b
loghmanb <loghman@motorway.co.uk>
2024-02-27 06:59:10 +0000

    fix typing

--- dynaportal/templatetags/checkboxes.py
+++ dynaportal/templatetags/checkboxes.py
@@ -1,4 +1,5 @@
 """Module for Checkboxes component."""
+from typing import Dict
 from django import template
 
 from ..common import Field
@@ -9,7 +10,7 @@ register = template.Library()
 @register.inclusion_tag(
         'dynaportal/components/checkboxes.html',
         takes_context=True)
-def checkboxes(context: dict, field: Field) -> dict[str, Field]:
+def checkboxes(context: Dict, field: Field) -> dict[str, Field]:
     """Checkboxes tag."""
     context['field'] = field
     return context
