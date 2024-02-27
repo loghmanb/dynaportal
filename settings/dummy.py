baa8b5241a2ee4afd23324701f8ac87a4254e541
loghmanb <loghman@motorway.co.uk>
2024-02-27 20:04:14 +0000

    refactor the code

---
+++ settings/dummy.py
@@ -0,0 +1,13 @@
+"""Dummy settings."""
+import os
+
+from settings.base import *  # noqa
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
