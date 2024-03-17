552ba8a09bdd14f088d79fbf4f61bf3a6428a474
loghmanb <loghman@motorway.co.uk>
2024-03-17 20:16:52 +0000

    add field-values and selected field and state and add value_label filter

--- dynaportal/templatetags/common.py
+++ dynaportal/templatetags/common.py
@@ -1,8 +1,12 @@
 """Module for common tags and filters."""
 
-from typing import Dict, List, Union
+import json
+from typing import Any, Dict, List, Union
 
 from django import template
+
+from ..common import Field, FieldType
+
 
 register = template.Library()
 
@@ -52,3 +56,22 @@ def item(options: Union[Dict, List], idx: Union[int, str], default_value=None) -
             return options[idx]
 
     return default_value
+
+@register.filter(is_safe=True)
+def value_label(field: Field, value: Any=None) -> Any:
+    """valueLabel filter."""
+    if value is not None:
+        if field.field_type in (FieldType.RADIOS, FieldType.SELECT):
+            found_items = list(filter(lambda x:x['value']==value, field.options['items']))
+            if found_items:
+                return found_items[0]['caption']
+            else:
+                return '%(value)s is Not valid value!' % { 'value': value }
+        elif field.field_type == FieldType.CHECKBOXES:
+            pass
+    return value
+
+@register.filter(is_safe=True)
+def values(fields: List[Field]) -> str:
+    result = { field.name: field.value for field in fields}
+    return json.dumps(result)
